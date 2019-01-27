80f3a9f3391f0f055f772f9bc26b563001cad0a3
JuuichiHirao <hirao@theta-inc.co.jp>
2019-01-27 21:30:56 +0900

    wikiにgoogle検索を追加、確認はconfirm_fanzaに追加

--- confirm_fanza.py
+++ confirm_fanza.py
@@ -3,10 +3,48 @@ from src import tool
 from src import db
 
 jav_dao = db.jav.JavDao()
+import_dao = db.import_dao.ImportDao()
+# maker_dao = db.maker.MakerDao()
 
-javs = jav_dao.get_where_agreement('WHERE id = 15867') # MACB-001
+# makers = maker_dao.get_all()
+google = site.wiki.Google()
+mgs = site.mgs.Mgs()
+
+# javs = jav_dao.get_where_agreement('WHERE is_selection = 1 limit 20')
+'''
 fanza = site.fanza.Fanza()
 p_tool = tool.p_number.ProductNumber()
 # p_number, seller, sell_date, match_maker, ng_reason = p_tool.parse_and_fc2(javs[0], True)
 site_data = fanza.get_info('SDMU-912')
 # site_data.print()
+'''
+
+'''
+for jav in javs:
+    google.get_info(javs[0].productNumber)
+'''
+
+import_list = import_dao.get_where_agreement('WHERE id > 5100')
+for import_data in import_list:
+    if '[裏' in import_data.filename:
+        print('URA not target')
+        continue
+
+    if len(import_data.filename) <= 0:
+        print('filename is not setting')
+        continue
+
+    site_name, site_url = google.get_info(import_data.productNumber)
+    search_result = site_name + ' ' + site_url
+    # print('result ' + site_name + ' ' + site_url)
+    # print('search_result ' + import_data.searchResult)
+
+    # if import_data.searchResult is not None and 'no search result' in import_data.searchResult:
+    import_dao.update_search_result(search_result, import_data.id)
+
+    '''
+        filter_makers = list(filter(lambda maker:
+                                         len(maker.matchProductNumber.strip()) > 0
+                                         and len(maker.matchStr) > 0, self.makers))
+    '''
+
--- src/site/wiki.py
+++ src/site/wiki.py
@@ -1,6 +1,133 @@
+import re
 import urllib.request
 from bs4 import BeautifulSoup
-from javcore import db
+from .. import db
+from .. import common
+from .. import data
+
+
+class Google:
+
+    def __init__(self, max_result: int = 5):
+        self.main_url = 'https://www.google.com/search?q=wiki+'
+        self.opener = urllib.request.build_opener()
+        self.opener.addheaders = [('User-Agent',
+                                   'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/36.0.1941.0 Safari/537.36')]
+
+        self.env = common.Environment()
+        self.driver = self.env.get_driver()
+        self.import_dao = db.import_dao.ImportDao()
+
+        self.max_result = max_result
+
+    def get_info(self, product_number):
+
+        url = self.main_url + product_number
+
+        urllib.request.install_opener(self.opener)
+
+        site_name = ''
+        site_url = ''
+        with urllib.request.urlopen(url) as response:
+            html = response.read()
+            google_result_soup = BeautifulSoup(html, "html.parser")
+            div_r_list = google_result_soup.findAll('div', class_='r')
+
+            for idx, div_r in enumerate(div_r_list):
+                a_div_r = div_r.find('a')
+                url = a_div_r['href']
+
+                print('Google ' + product_number + ' ' + url)
+
+                if 'shecool.net' in url:
+                    site_name = 'shecool.net'
+                    site_url = url
+                    break
+
+                if 'sougouwiki.com' in url and '/d/' in url:
+                    site_name = '総合wiki'
+                    site_url = url
+                    break
+
+                if 'seasaawiki.jp' in url:
+                    site_name = 'seasaaWiki'
+                    site_url = url
+                    break
+
+                if 'avwikich.com' in url:
+                    site_name = 'AVWikiCh'
+                    site_url = url
+                    break
+
+                if idx > 8:
+                    break
+
+        return site_name, site_url
+
+    def __parse_site_data(self, url):
+
+        site_data = None
+        with urllib.request.urlopen(url) as response:
+            html = response.read()
+            html_soup = BeautifulSoup(html, "html.parser")
+
+            table_info = html_soup.find('table', class_='mg-b20')
+            # tr_all = table_info.findAll('tr')
+            tr_all = table_info.findAll('tr')
+            site_data = data.SiteData()
+            h1_title = html_soup.find('h1', id='title')
+            if h1_title:
+                site_data.title = h1_title.text
+            before_name = ''
+            for idx, tr in enumerate(tr_all):
+                for idx_sub, td in enumerate(tr.find_all_next('td')):
+                    # print(str(idx) + ' ' + str(idx_sub) + ' 【' + str(td.text) + '】')
+
+                    if before_name == '発売日':
+                        site_data.streamDate = td.text.strip()
+                        before_name = ''
+                    if re.search('発売日', td.text) or re.search('配信.*日', td.text):
+                        before_name = '発売日'
+
+                    if before_name == 'duration':
+                        site_data.duration = td.text.replace('\n', ' ').strip()
+                        before_name = ''
+                    if re.search('収録時間', td.text):
+                        before_name = 'duration'
+
+                    if before_name == 'actress':
+                        site_data.actress = td.text.replace('\n', '').strip()
+                        before_name = ''
+                    if re.search('出演者', td.text):
+                        before_name = 'actress'
+
+                    if before_name == 'maker':
+                        site_data.maker = td.text.strip()
+                        before_name = ''
+                    if re.search('メーカー', td.text):
+                        before_name = 'maker'
+
+                    if before_name == 'label':
+                        site_data.label = td.text.strip()
+                        before_name = ''
+                    if re.search('レーベル', td.text):
+                        before_name = 'label'
+
+                    if before_name == 'series':
+                        site_data.series = td.text.strip()
+                        before_name = ''
+                    if re.search('シリーズ', td.text):
+                        before_name = 'series'
+
+                # print('tr end')
+                # site_data.print()
+                break
+
+        # site_data.print()
+
+        urllib.request.urlcleanup()
+
+        return site_data
 
 
 class SougouWiki:
